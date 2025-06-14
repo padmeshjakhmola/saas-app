f61c0472ad7bbe472da3a5fbae86a226f0121897
Padmesh Jakhmola <prankshadow@gmail.com>
2025-06-14 10:59:03 +0530

    feat: created card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas App</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Polpular Companion</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="789"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#BDE7FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          subject="language"
+          duration={50}
+          color="#FFDA6E"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
